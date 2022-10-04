4af8fd3d3ee809ab1d156576c5896023f2661e92
Sócrates Manaure <smanaure93@gmail.com>
2022-10-04 11:13:08 -0400

    vid-16-functions-exercises

---
+++ clean-course-main/src/clean-code/03-functions.ts
@@ -0,0 +1,56 @@
+(() => {
+  // función para obtener información de una película por Id
+  //   function getAllMovies(movieId: string) {
+  //     console.log({ movieId });
+  //   }
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  //   function getAllMovieActors(id: string) {
+  //     console.log({ id });
+  //   }
+  function getMovieCast(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  //   function getUsuario(ActorId: string) {
+  //     console.log({ ActorId });
+  //   }
+  function getActorBio(actorId: string) {
+    console.log({ actorId });
+  }
+
+  // Crear una película
+  //   function createMovie(
+  //     title: string,
+  //     description: string,
+  //     rating: number,
+  //     cast: string[]
+  //   ) {
+  //     console.log({ title, description, rating, cast });
+  //   }
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "Sócrates") return false;
+
+    console.log(`Crear actor: ${fullName} nacio el ${birthdate.toString()}`);
+    return true;
+  }
+})();
