f2a6d6e1234a05d4d3c8bc3b4b990cd693ff8059
Sócrates Manaure <smanaure93@gmail.com>
2022-10-04 12:25:18 -0400

    vid-21-dry

--- clean-course-main/src/main.ts
+++ clean-course-main/src/main.ts
@@ -1,10 +1,10 @@
-import './style.css'
-import './clean-code/04-homework';
+import "./style.css";
+import "./clean-code/04-homework";
+import "./clean-code/05-dry";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
