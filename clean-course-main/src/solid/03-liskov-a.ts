85ba40554b1b728050b1a9e33d61c8a9fba098eb
Sócrates Manaure <smanaure93@gmail.com>
2022-10-11 12:47:00 -0400

    vid-56-liskov

---
+++ clean-course-main/src/solid/03-liskov-a.ts
@@ -0,0 +1,21 @@
+import { Tesla, Audi, Toyota, Honda, Vehicle, Ford } from "./03-liskov-b";
+
+(() => {
+  const printCarSeats = (cars: Vehicle[]) => {
+    cars.forEach((car) => {
+      console.log(
+        `${car.constructor.name} with ${car.getNumberOfSeats()} seats`
+      );
+    });
+  };
+
+  const cars = [
+    new Tesla(7),
+    new Audi(2),
+    new Toyota(5),
+    new Honda(5),
+    new Ford(2),
+  ];
+
+  printCarSeats(cars);
+})();
